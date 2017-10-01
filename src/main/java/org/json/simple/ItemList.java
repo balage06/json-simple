bf6721564d34c00ea532c66605301c573a7c8218
Balázs Polgár <balage06@gmail.com>
2017-10-01 10:23:32 +0200

    Fixed build error

--- src/main/java/org/json/simple/ItemList.java
+++ src/main/java/org/json/simple/ItemList.java
@@ -40,7 +40,7 @@ public class ItemList {
 	}
 	
 	public String[] getArray(){
-		return this.items.toArray(new String[this.items.size()]);
+		return (String[])this.items.toArray();
 	}
 	
 	public void split(String s,String sp,List append,boolean isMultiToken){
